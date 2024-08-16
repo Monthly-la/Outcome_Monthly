94a72e100b6f8876e29b6736f6a4b207b3ecfa98
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-08-16 12:32:16 -0600

    Update Procesador.py

--- pages/Procesador.py
+++ pages/Procesador.py
@@ -142,6 +142,143 @@ def process_data(df, option = option):
 
         return outcome_df
 
+#Contpaqi
+    if option == "Contpaqi":
+        #Contpaqi Excel Sheets
+        tabs = list(df.keys())
+        tabs_dates = []
+
+        outcome_df = pd.DataFrame(["","","",""], ["Cuenta", "Nombre", "Saldo Neto", "Sheet"]).T
+
+
+        for tab in tabs:
+            df = pd.read_excel(uploaded_file, sheet_name = tab)
+            tabs_date = df["CONTPAQ i"].iloc[0][-8:]
+            tabs_dates.append(tabs_date)
+            df = df.fillna(0)
+            df = df.replace(' ',0)
+            
+            df.columns = ["Cuenta", "Nombre"]+list(df.loc[4][2:])
+            df = df.iloc[6:-7].reset_index().drop(columns = ["index"])
+            
+            type_of_Cuenta = []
+            for i in list(df["Cuenta"]):
+                type_of_Cuenta.append(type(i))
+                
+            df["Type"] = type_of_Cuenta
+            df = df[df["Type"] == type("str")]
+            df = df[:-1]
+            df = df[df["Cuenta"] != "Fecha"]
+            
+            nombre_list = []
+            for n in df["Nombre"]:
+                nombre_list.append(n.strip())
+            
+            df["Nombre"] = nombre_list
+            
+            nivel_list = []
+            for i in list(df["Cuenta"]):
+                if (i.split("-")[2] == '000'):
+                    if ((i.split("-")[1] == '00') or (i.split("-")[1] == '000')) and (i.split("-")[2] == '000'):
+                        if (i.split("-")[0][-1:] == '0') and ((i.split("-")[1] == '00') or (i.split("-")[1] == '000')) and (i.split("-")[2] == '000'):
+                            if (i.split("-")[0][-2:] == '00') and ((i.split("-")[1] == '00') or (i.split("-")[1] == '000')) and (i.split("-")[2] == '000'):
+                                if (i.split("-")[0][-3:] == '000') and ((i.split("-")[1] == '00') or (i.split("-")[1] == '000')) and (i.split("-")[2] == '000'):
+                                    nivel_list.append(1)
+                                else:
+                                    nivel_list.append(2)
+                            else:
+                                nivel_list.append(3)
+                        else:
+                            nivel_list.append(4)
+                    else:
+                        nivel_list.append(5)
+                else:
+                    nivel_list.append(6)
+            
+            
+            df["Nivel"] = nivel_list
+            class_of_cuenta = []
+            for i in list(df["Cuenta"]):
+                class_of_cuenta.append(i[0])
+            
+            df["Class"] = class_of_cuenta
+            df["Class"] = df["Class"].astype("int")
+            
+            df.columns = ["Cuenta", "Nombre", "Saldo Inicial Deudor", "Saldo Inicial Acreedor", "Cargos", "Abonos", "Saldo Final Deudor", "Saldo Final Acreedor", "Tipo", "Nivel", "Clase"]
+            df["Saldo Final Deudor"] = df["Saldo Final Deudor"].astype("float")
+            df["Saldo Final Acreedor"] = df["Saldo Final Acreedor"].astype("float")
+            
+            df["Saldo Neto"] = df["Saldo Final Deudor"] - df["Saldo Final Acreedor"]
+            
+            activos = df[(df["Nivel"] == 1) & (df["Clase"] == 1)]["Saldo Neto"].sum()
+            pasivos = df[(df["Nivel"] == 1) & (df["Clase"] == 2)]["Saldo Neto"].sum()
+            capital= df[(df["Nivel"] == 1) & (df["Clase"] == 3)]["Saldo Neto"].sum()
+            utilidad_acum = df[(df["Nivel"] == 1) & (df["Clase"] >= 4)]["Saldo Neto"].sum()
+            
+            result = round(activos+pasivos+capital+utilidad_acum,1)
+            
+            results_df = pd.DataFrame({"Cuentas": ["Activos", "Pasivos", "Capital", "Utilidad Acumulada", "Sumatoria Final"], 
+                                    "Resultado":[activos, pasivos, capital, utilidad_acum, result]})
+            
+            if result == 0:
+                st.caption(tabs_date + " - Check ✅")
+            else:
+                st.caption(tabs_date + " - No Check ❌")
+            
+            st.dataframe(results_df, width = 1000)
+            st.divider()
+            
+            #Outcome Matrix
+
+            balance_df = df[(df["Nivel"] == 2) & (df["Clase"] <= 3)][["Cuenta","Nombre","Saldo Neto"]]
+            balance_df["Sheet"] = tabs_date
+
+            inc_statem_df = df[(df["Clase"] > 3)]
+            inc_statem_df = inc_statem_df.drop(columns = ["Saldo Neto"])
+            general = []
+
+            for i in inc_statem_df["Cuenta"]:
+                general.append(i[:4])
+            inc_statem_df["Cuenta General"] = general
+            
+            detalle_df = inc_statem_df.groupby(by = ["Cuenta General", "Nivel"])["Cuenta",].count().reset_index()
+            
+            nivel_deseado = []
+            for c in list(set(detalle_df["Cuenta General"])):
+                detalle_nivel_df = detalle_df[detalle_df["Cuenta General"] == c]
+                
+                if len(detalle_nivel_df[detalle_nivel_df["Cuenta"] >= 2]) > 0:
+                    nivel_deseado.append([c, max(detalle_nivel_df[detalle_nivel_df["Cuenta"] >= 2]["Nivel"])])
+                else:
+                    nivel_deseado.append([c, 1])   
+            nivel_deseado_df = pd.DataFrame(nivel_deseado, columns = ["Cuenta General", "Nivel Deseado"])
+            
+            detalle_deseado_df = detalle_df.merge(nivel_deseado_df, on = "Cuenta General", how = "left")[["Cuenta General", "Nivel Deseado"]].drop_duplicates()
+            
+            inc_statem_df = inc_statem_df.merge(detalle_deseado_df, on = "Cuenta General", how = "left")
+            inc_statem_df["Saldo Neto"] = df["Cargos"] - df["Abonos"]
+            
+            inc_statem_df = inc_statem_df[inc_statem_df["Nivel"] == inc_statem_df["Nivel Deseado"]]
+            inc_statem_df = inc_statem_df[["Cuenta", "Nombre", "Saldo Neto"]]
+            inc_statem_df["Sheet"] = tabs_date
+
+            outcome_df = pd.concat([outcome_df, balance_df, inc_statem_df])
+        
+        outcome_df = outcome_df.pivot(index=["Cuenta", "Nombre"], columns=["Sheet"], values=["Saldo Neto"]).iloc[1:].reset_index().droplevel(0, axis = 1)
+        outcome_df = outcome_df.fillna(0)
+
+        outcome_columns = ["Cuenta", "Nombre"]
+
+        for i in outcome_df.columns[2:]:
+            outcome_columns.append(i)
+
+        outcome_df.columns = outcome_columns
+        outcome_df = outcome_df[["Cuenta", "Nombre"] + tabs_dates]
+
+        outcome_df["Cuenta"] = outcome_df["Cuenta"].astype("str")
+        
+        return outcome_df
+
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file, sheet_name = None)
