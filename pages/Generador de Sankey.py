fd5c056a8b5601e9e9886e591677020a1228dae3
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-08-20 15:04:36 -0600

    Update Generador de Sankey.py

--- pages/Generador de Sankey.py
+++ pages/Generador de Sankey.py
@@ -67,9 +67,8 @@ def get_website_content(url, df, df_final, df_concept_colors):
                 cell = "//div[@class='dsg-row'][@style='"+ r +"']//div[@class='dsg-cell']//input[@class='dsg-input']"
                 #cell = "//div[@class='dsg-row'][@style='"+ r +"']//div[@class='dsg-cell'][@style='"+ c +"']//input[@class='dsg-input']"
 
-                st.markdown(cell)
-                cell_field = driver.find_elements("xpath", cell)
-                st.markdown(cell_field)
+                cell_field = driver.find_elements("xpath", cell[c_counter])
+                st.markdown(cell_field.text)
                 st.markdown(len(cell_field))
                 #cell_field = WebDriverWait(driver, 2).until(ec.element_to_be_clickable((By.CSS_SELECTOR, cell))).click()
                 ActionChains(driver).click(cell_field[0]).perform()
