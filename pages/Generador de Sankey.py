266885fcc798394e2c536e70cc32cec9315ff685
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-08-20 10:49:48 -0600

    Update Generador de Sankey.py

--- pages/Generador de Sankey.py
+++ pages/Generador de Sankey.py
@@ -1,8 +1,10 @@
 import streamlit as st
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+from bs4 import BeautifulSoup
 
 
 st.set_page_config(
@@ -23,42 +25,50 @@ with header_logo_2:
 st.divider()
 
 
-# Function to initialize WebDriver
-def init_driver():
-    # Using ChromeDriverManager to handle WebDriver setup
-    service = Service(ChromeDriverManager().install())
-    
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    # Specify the correct path to the Chrome binary
-    chrome_binary_path = "./chrome.exe"  # Update this path if incorrect
-    options.binary_location = chrome_binary_path
-    
-    driver = webdriver.Chrome(service=service, options=options)
-    return driver
-
-# Streamlit app
-st.title("Selenium with Streamlit and WebDriver Manager")
-
-if st.button("Run Web Scraping"):
-    st.write("Initializing WebDriver...")
+def get_website_content(url):
+    driver = None
     try:
-        driver = init_driver()
-        
-        # Example of accessing a webpage
-        driver.get("https://www.example.com")
-        st.write("Accessed the website")
-        
-        # Example of finding an element
-        heading = driver.find_element(By.TAG_NAME, "h1").text
-        st.write("Heading found:", heading)
-        
-        # Close the driver
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+        time.sleep(5)
+        html_doc = driver.page_source
         driver.quit()
-        st.write("WebDriver session closed.")
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup.get_text()
     except Exception as e:
-        st.error(f"An error occurred: {e}")
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
 
+
+
+
+# ---------------- Page & UI/UX Components ------------------------
+def main_sidebar():
+    # 1.Vertical Menu
+    st.header("Running Selenium on Streamlit Cloud")
+    site_extraction_page()
+
+
+def site_extraction_page():
+    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
+    url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
+
+    clicked = st.button("Load Page Content",type="primary")
+    if clicked:
+        with st.container(border=True):
+            with st.spinner("Loading page website..."):
+                content = get_website_content(url)
+                st.write(content)
+
+
+if __name__ == "__main__":
+    main_sidebar()
