a2ff6459a9d95c715a3b592cb14d9c2c08af8d7d
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-08-21 12:17:46 -0600

    Update Generador de Sankey.py

--- pages/Generador de Sankey.py
+++ pages/Generador de Sankey.py
@@ -154,8 +154,9 @@ def get_website_content(url, df):
         st.markdown("Uniendo Colores a Nodos")
         st.markdown("Prueba 1: " + str(df_concept_colors["Color"][0]))
         df_concept_colors = order.merge(df_concept_colors, on = "Concepto", how = "left")
-        df_concept_colors = df_concept_colors.dropna(axis='rows').reset_index().drop(columns = "index")
         st.markdown("Prueba 2: " + str(df_concept_colors["Color"][0]))
+        df_concept_colors = df_concept_colors.dropna(axis='rows').reset_index().drop(columns = "index")
+        st.markdown("Prueba 3: " + str(df_concept_colors["Color"][0]))
         st.markdown("Colores Definidos en Nodos")
         
         for c in range(len(nodes_location)):
@@ -163,7 +164,7 @@ def get_website_content(url, df):
             element = nodes_location[c].find_element("tag name", "rect")
             st.markdown(element)
             st.markdown(element.get_attribute('fill'))
-            st.markdown("Prueba 3: " + str(df_concept_colors["Color"][c]))
+            st.markdown("Prueba 4: " + str(df_concept_colors["Color"][c]))
             st.markdown("Hola")
             driver.execute_script("arguments[0].setAttribute('fill', '" + df_concept_colors["Color"][c] + "')", element)
   
