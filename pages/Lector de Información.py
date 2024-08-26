b458982d16e3be5ff8b136142ab3c9651d42495d
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-08-26 13:01:15 -0600

    Update Lector de Información.py

--- pages/Lector de Información.py
+++ pages/Lector de Información.py
@@ -1,1 +1,26 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Monthly - App Interna",
+    page_icon="💸",
+    layout="wide"
+)
+
+st.markdown("""
+<style>
+button {
+    background-color: #14E79D;
+}
+</style>
+""", unsafe_allow_html=True)
+
+header_logo_1, header_logo_2 = st.columns(2)
+with header_logo_1:
+    st.image(
+                "https://monthly.la/wp-content/uploads/2024/02/Monthly-Logo.png",
+                width=200, # Manually Adjust the width of the image as per requirement
+            )
+with header_logo_2:
+    st.markdown("<h2 style='text-align: right; color: #5666FF;'>🔎 Lector de Información</h2>", unsafe_allow_html=True)
+
+st.divider()
