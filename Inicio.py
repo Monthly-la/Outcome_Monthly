f3e9c591c0c8bcfbc18091045aeed29f1a9a15a1
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-09-19 11:19:49 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -37,8 +37,8 @@ with cl_venta:
     st.markdown("<h3 style='text-align: center; color: #5666FF;'> Venta</h3>", unsafe_allow_html=True)
     st.markdown("")
     st.link_button("Linktree (Start)", "https://linktr.ee/monthly.start", use_container_width=True)
-    #st.link_button("PDF Comercial - Brochure", "", use_container_width=True)
-    #st.link_button("PDF Comercial - Presentación", "", use_container_width=True)
+    st.link_button("PDF Comercial - Brochure", "", use_container_width=True)
+    st.link_button("PDF Comercial - Presentación", "", use_container_width=True)
     st.link_button("Notion (Payment Links)", "https://www.notion.so/Stripe-links-4a6b8046b85d4959a458239ff184020c", use_container_width=True)
     st.link_button("CRM", "https://airtable.com/app2GD2pTpJAZ8JEq/tblmo0rAeoOju6yYu/viw0d5JhbaF9EbCcp?blocks=hide", use_container_width=True)
     st.link_button("Fillout (Subscripción de Cliente)", "https://forms.fillout.com/t/xrzArzx8guus", use_container_width=True)
@@ -54,24 +54,27 @@ with cl_operaciones:
     st.markdown("<h3 style='text-align: center; color: #5666FF;'> Operaciones</h3>", unsafe_allow_html=True)
     st.markdown("")
     st.link_button("Plataforma Interna", "https://outcome-monthly.streamlit.app/", use_container_width=True)
-    #st.link_button("Manual de Nuevo Reporte", "", use_container_width=True)
+    st.link_button("Manual de Nuevo Reporte", "", use_container_width=True)
     st.link_button("Excel (Graficación)", "https://monthlyla.sharepoint.com/:x:/s/monthly.la/EWB3hy7QuFBPp8bE4fJOhIwBb3MxBX8ZHjMPeSg6ZYmfsA?e=eJE90X", use_container_width=True)
-    #st.link_button("Ppt (ReporteCFO)", "", use_container_width=True)
+    st.link_button("Ppt (ReporteCFO)", "", use_container_width=True)
     st.link_button("Sankey", "https://www.sankeyart.com/", use_container_width=True)
     st.link_button("Fillout (Documentos Mensuales)", "https://forms.fillout.com/t/vzB2cu9gHDus", use_container_width=True)
-    #st.link_button("ChatGPT (Monthly)", "", use_container_width=True)
+    st.link_button("ChatGPT (Monthly)", "", use_container_width=True)
     st.link_button("Weekly Pulse (TGIMⓂ️)", "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=tk7h7DvZskO0b7FLepIvPWStz4K_-KlJj4uTVyohkktUOU9OMEM3Tk5EQ1NaTUhZQzNYMFBNQjc0VC4u&topview=Prefill", use_container_width=True)
-    
-    
-    
-
 
 with cl_postventa:
     st.markdown("<h3 style='text-align: center; color: #5666FF;'> Postventa</h3>", unsafe_allow_html=True)
     st.markdown("")
     st.link_button("Link Plataforma", "https://outcome-monthly.streamlit.app/", use_container_width=True)
-    #st.link_button("Documento de Errores Comunes", "", use_container_width=True)
-    #st.link_button("Manual de Nuevo Reporte", "", use_container_width=True)
+    st.link_button("Documento de Errores Comunes", "", use_container_width=True)
+    st.link_button("Manual de Nuevo Reporte", "", use_container_width=True)
+
+with cl_recursos:
+    st.markdown("<h3 style='text-align: center; color: #5666FF;'> Recursos</h3>", unsafe_allow_html=True)
+    st.markdown("")
+    st.link_button("Cursos de Excel", "", use_container_width=True)
+    st.link_button("Cursos de PPT", "", use_container_width=True)
+    st.link_button("Manual Financiero", "https://www.notion.so/Monthly-Way-El-iQ-del-CFO-Virtual-48e24fbc16334c109db479810d88e2b3", use_container_width=True)
 
 st.markdown("")
 st.divider()
