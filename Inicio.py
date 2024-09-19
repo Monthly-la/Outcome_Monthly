a8adcf814edec995bc147d565d7fb666f6cc5ce5
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-09-19 10:32:51 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -17,11 +17,10 @@ with header_logo_2:
 
 st.divider()
 
-st.image("Monthly. Arquitectura de Datos.svg")
-
 cl_prospeccion, cl_venta, cl_onboarding, cl_operaciones, cl_postventa, cl_recursos = st.columns(6)
 
 with cl_prospeccion:
+    st.markdown("<h3 style='text-align: right; color: #5666FF;'>Prospección</h3>", unsafe_allow_html=True)
     st.link_button("Substack", "https://visualcfo.substack.com/")
     st.link_button("Linktree Monthly", "https://linktr.ee/monthlyla")
     st.link_button("Linktree Valora", "https://linktr.ee/valora.la")
@@ -33,10 +32,12 @@ with cl_prospeccion:
     
     
 with cl_venta:
+    st.markdown("<h3 style='text-align: right; color: #5666FF;'>Venta</h3>", unsafe_allow_html=True)
     st.link_button("Linktree Start", "https://linktr.ee/monthly.start")
     #st.link_button("PDF Comercial", "")
     st.link_button("Payment Links", "https://www.notion.so/Stripe-links-4a6b8046b85d4959a458239ff184020c")
     st.link_button("CRM", "https://airtable.com/app2GD2pTpJAZ8JEq/tblmo0rAeoOju6yYu/viw0d5JhbaF9EbCcp?blocks=hide")
     st.link_button("Fillout (Subscripción de Cliente)", "https://forms.fillout.com/t/xrzArzx8guus")
-    
+
+st.image("Monthly. Arquitectura de Datos.svg")
     
