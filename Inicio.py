bb6d35a1163adb0a279e876c418048ce939e2c81
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-09-19 10:33:51 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -16,11 +16,13 @@ with header_logo_2:
     st.markdown("<h2 style='text-align: right; color: #5666FF;'>🏠 Inicio</h2>", unsafe_allow_html=True)
 
 st.divider()
+st.markdown("")
 
 cl_prospeccion, cl_venta, cl_onboarding, cl_operaciones, cl_postventa, cl_recursos = st.columns(6)
 
 with cl_prospeccion:
     st.markdown("<h3 style='text-align: right; color: #5666FF;'>Prospección</h3>", unsafe_allow_html=True)
+    st.markdown("")
     st.link_button("Substack", "https://visualcfo.substack.com/")
     st.link_button("Linktree Monthly", "https://linktr.ee/monthlyla")
     st.link_button("Linktree Valora", "https://linktr.ee/valora.la")
@@ -33,6 +35,7 @@ with cl_prospeccion:
     
 with cl_venta:
     st.markdown("<h3 style='text-align: right; color: #5666FF;'>Venta</h3>", unsafe_allow_html=True)
+    st.markdown("")
     st.link_button("Linktree Start", "https://linktr.ee/monthly.start")
     #st.link_button("PDF Comercial", "")
     st.link_button("Payment Links", "https://www.notion.so/Stripe-links-4a6b8046b85d4959a458239ff184020c")
