a7eb2a35a23e94f44ce8f718bcb6c17b6ab2ef83
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-09-19 12:20:22 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -18,7 +18,7 @@ with header_logo_2:
 st.divider()
 st.markdown("")
 
-st.markdown("<h3 style='text-align: center; color: #14E79D​;'> Prospección</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: #14E79D​;'> Modelo de Servicio Monthly</h3>", unsafe_allow_html=True)
 st.markdown("")
 
 cl_prospeccion, cl_venta, cl_onboarding, cl_operaciones, cl_postventa, cl_recursos = st.columns(6)
