8a3221ce8cee94c5d1afd45c5ef4d07d6d830cc0
Monthly-la <160193865+Monthly-la@users.noreply.github.com>
2024-10-20 13:40:18 -0600

    Update Gestión de Efectivo.py

--- pages/Gestión de Efectivo.py
+++ pages/Gestión de Efectivo.py
@@ -1,1 +1,33 @@
+import streamlit as st
+import pyexcel as p
+import pandas as pd
+from io import BytesIO
+
+
+st.set_page_config(
+    page_title="Monthly - App Interna",
+    page_icon="💸",
+    layout="wide"
+)
+primary_clr = st.get_option("theme.primaryColor")
+
+
+st.markdown("""
+<style>
+button {
+    background-color: #14E79D;
+}
+</style>
+""", unsafe_allow_html=True)
+
+header_logo_1, header_logo_2 = st.columns(2)
+with header_logo_1:
+    st.image(
+                "https://monthly.la/wp-content/uploads/2024/02/Monthly-Logo.png",
+                width=200, # Manually Adjust the width of the image as per requirement
+            )
+with header_logo_2:
+    st.markdown("<h2 style='text-align: right; color: #5666FF;'>Gestión de Efectivo</h2>", unsafe_allow_html=True)
+
+st.divider()
 
