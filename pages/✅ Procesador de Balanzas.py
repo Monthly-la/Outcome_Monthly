2b01ef5832422e6ae4d11bbc1ba0ab06871b5495
Checo <160193865+Monthly-la@users.noreply.github.com>
2024-11-06 13:12:17 -0600

    Update ✅ Procesador de Balanzas.py

--- pages/✅ Procesador de Balanzas.py
+++ pages/✅ Procesador de Balanzas.py
@@ -313,8 +313,8 @@ def process_data(df, option = option):
             st.divider()
             
             #Outcome Matrix
-            
-            balance_df = df[(df["Nivel"] == min(list(set(df["Nivel"] <= 3)))) & (df["Clase"] <= 3)][["Cuenta","Nombre","Saldo Neto"]]
+            lowest = min(list(set(list(df["Nivel"] <= 3))))
+            balance_df = df[(df["Nivel"] == lowest) & (df["Clase"] <= 3)][["Cuenta","Nombre","Saldo Neto"]]
             balance_df["Sheet"] = tabs_date
 
             inc_statem_df = df[(df["Clase"] > 3)]
