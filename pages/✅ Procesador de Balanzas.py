82866f106d7aecca2bbc3faa269cc45a55f8207d
Checo <160193865+Monthly-la@users.noreply.github.com>
2024-11-06 12:08:56 -0600

    Update ✅ Procesador de Balanzas.py

--- pages/✅ Procesador de Balanzas.py
+++ pages/✅ Procesador de Balanzas.py
@@ -348,7 +348,7 @@ def process_data(df, option = option):
             
             inc_statem_df = inc_statem_df[inc_statem_df["Nivel"] == inc_statem_df["Nivel Deseado"]]
 
-            if len(inc_statem_df) > 1:
+            if len(inc_statem_df) > 2:
                 inc_statem_df = inc_statem_df[inc_statem_df["Nivel"] != 1]
             
             inc_statem_df = inc_statem_df[["Cuenta", "Nombre", "Saldo Neto"]]
