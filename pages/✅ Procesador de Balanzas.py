068836d01850908266e51080f756ad66621f05b7
Checo <160193865+Monthly-la@users.noreply.github.com>
2025-03-25 14:03:04 -0600

    Update ✅ Procesador de Balanzas.py

--- pages/✅ Procesador de Balanzas.py
+++ pages/✅ Procesador de Balanzas.py
@@ -380,18 +380,18 @@ def process_data(df, option = option):
             inc_statem_df = inc_statem_df.drop(columns = ["Saldo Neto"])
             general = []
 
-            cuenta_general_corte = int(sorted(inc_statem_df['Nivel'].unique())[-1])
-
-            st.markdown(cuenta_general_corte)
             
-            #try:
-            #    cuenta_general_corte = set[list[inc_statem_df["Nivel"]]][-3]
-            #except:
-            #    cuenta_general_corte = set[list[inc_statem_df["Nivel"]]][-2]
+            try:
+                cuenta_general_corte = int(sorted(inc_statem_df['Nivel'].unique())[-3])
+
+            except:
+                cuenta_general_corte = int(sorted(inc_statem_df['Nivel'].unique())[-3])
+            st.markdown(cuenta_general_corte)
+
                 
             
             for i in inc_statem_df["Cuenta"]:
-                general.append(i[:cuenta_general_corte])
+                general.append(i[:2])
 
             inc_statem_df["Cuenta General"] = general
             
