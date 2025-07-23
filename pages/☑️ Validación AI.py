ac01a2465e27cc92eac266388d937441da280edd
Checo <160193865+Monthly-la@users.noreply.github.com>
2025-07-23 16:02:58 -0600

    Update ☑️ Validación AI.py

--- pages/☑️ Validación AI.py
+++ pages/☑️ Validación AI.py
@@ -45,7 +45,7 @@ if pptx_file:
 
     try:
         response = client.chat.completions.create(
-            model="gpt-4-vision-preview",
+            model="gpt-4o",
             messages=[
                 {
                     "role": "system",
